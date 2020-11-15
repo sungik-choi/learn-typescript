189ca9ce181a3d864b2eae53fb458fda3d5a3014
Sungik Choi <tjddlr92@gmail.com>
2020-11-15 17:34:50 +0900

    introduce

--- why-ts/app.js
+++ why-ts/app.js
@@ -1,21 +1,38 @@
 // api url
-var url = 'https://jsonplaceholder.typicode.com/users/1';
+var url = "https://jsonplaceholder.typicode.com/users/1";
 
 // dom
-var username = document.querySelector('#username');
-var email = document.querySelector('#email');
-var address = document.querySelector('#address');
+var username = document.querySelector("#username");
+var email = document.querySelector("#email");
+var address = document.querySelector("#address");
 
 // user data
 var user = {};
 
+/**
+ * @typedef {object} User
+ * @property {string} name
+ * @property {string} email
+ * @property {string} address
+ */
+
+/**
+ * @returns {Promise<User>}
+ */
+
+function fetchUser() {
+  return axios.get(url);
+}
+
 function startApp() {
-  axios
-    .get(url)
+  fetchUser()
     .then(function (response) {
       console.log(response);
       user = response.data;
-      // TODO: 이름, 이메일, 주소 표시하기
+
+      username.innerText = user.name;
+      email.innerText = user.email;
+      address.innerText = user.address.street;
     })
     .catch(function (error) {
       console.log(error);
